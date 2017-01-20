af62d43533e6253b415d29e496bd063769276485
CarltonYang <carltonyang@gmail.com>
2017-01-20 10:16:45 +0800

    context.cpp 1/3 update

--- source/context.cpp
+++ source/context.cpp
@@ -20,7 +20,7 @@ double context::calculateNeighbourAvg(specie sp, int time){
     // For each mRNA concentration, average the given cell's neighbors' Delta protein concentrations
     int* cells = _simulation._neighbors[_cell];
     //int time = WRAP(_simulation._j - delay, _simulation._delay_size[sp.index]);
-    concentration_level<double>::cell cur_cons = cl[CPDELTA][time];
+    concentration_level<double>::cell cur_cons = _simulation._cl[CPDELTA][time];
     double sum=0;
     if (_cell % _simulation.width_total == _simulation.active_start_record[time]) {
         sum = (cur_cons[cells[0]] + cur_cons[cells[3]] + cur_cons[cells[4]] + cur_cons[cells[5]]) / 4;
@@ -37,13 +37,14 @@ double[] calculateRates(){
     
 }
 
-void context::updateCon(concentration_level& cl, double[] rates){
+void context::updateCon(double[] rates){
     //double step_size= _simulation.step_size;
-    int j= _simulation._j;
+    
     double curr_rate=0;
     for (int i=0; i< NUM_SPECIES; i++){
         curr_rate= rates[i];
-        cl[i][j+1][_cell]=cl[i][j][cell]+ _simulation.step_size* curr_rate;
+        int baby_j= _simulation._baby_j[i];
+        _simulation._cl[i][baby_j+1][_cell]=_simulation._cl[i][baby_j][cell]+ _simulation.step_size* curr_rate;
     }
     
 }
