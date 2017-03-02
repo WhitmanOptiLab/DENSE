db1f1f10511c706dbdb949deeb6f6a589d657e8b
CarltonYang <carltonyang@gmail.com>
2017-03-02 15:57:47 -0500

    fixed index for baby_cl in context.hpp

--- source/context.hpp
+++ source/context.hpp
@@ -21,7 +21,7 @@ class Context {
   RATETYPE getCon(specie_id sp, int delay = 0) {
     // FIXME: calculate an actual time, not just using the delay, and make sure the indexes are 
     // in the right order
-    return _simulation._baby_cl[_cell][delay][sp];
+    return _simulation._baby_cl[sp][delay][_cell];
   }
 };
 
