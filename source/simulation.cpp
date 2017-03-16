d9446a7d11c4fd56e099f604dc2e98fa88ba4edb
Stratton John <strattja@strattja.whitman.edu>
2017-03-16 15:54:48 -0700

    Removed unused gradient from calc_max_delays

--- source/simulation.cpp
+++ source/simulation.cpp
@@ -226,12 +226,12 @@ void simulation::calc_max_delays() {
   //    accumulate delay into specie
   //  for each factor
   //    accumulate delay into specie
+  //RATETYPE max_gradient_##name = 0; \
+  //for (int k = 0; k < _width_total; k++) { \
+  //  max_gradient_##name = std::max<int>(_model.factors_gradient[ name ][k], max_gradient_##name); \
+  //} 
 #define REACTION(name) 
 #define DELAY_REACTION(name) \
-  RATETYPE max_gradient_##name = 0; \
-  for (int k = 0; k < _width_total; k++) { \
-    max_gradient_##name = std::max<int>(_model.factors_gradient[ name ][k], max_gradient_##name); \
-  } \
   for (int in = 0; in < _model.reaction_##name.getNumInputs(); in++) { \
     int& sp_max_delay = max_delays[_model.reaction_##name.getInputs()[in]]; \
     sp_max_delay = std::max<int>(_parameter_set._delay_sets[ dreact_##name ] * (1.0 + _model.factors_perturb[ name ] ), sp_max_delay); \
@@ -239,7 +239,7 @@ void simulation::calc_max_delays() {
   for (int in = 0; in < _model.reaction_##name.getNumFactors(); in++) { \
     int& sp_max_delay = max_delays[_model.reaction_##name.getFactors()[in]]; \
     sp_max_delay = std::max<int>(_parameter_set._delay_sets[ dreact_##name ] * (1.0 + _model.factors_perturb[ name ] ), sp_max_delay); \
-  } 
+  }
 #include "reactions_list.hpp"
 #undef REACTION
 #undef DELAY_REACTION
