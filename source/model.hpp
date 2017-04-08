db4890f17e83b24aef0829e347fca351169f7dc1
CarltonYang <carltonyang@gmail.com>
2017-04-08 19:51:33 -0400

    commit after merge

--- source/model.hpp
+++ source/model.hpp
@@ -27,15 +27,7 @@ private:
     RATETYPE* factors_gradient[NUM_REACTIONS];
     bool _has_gradient[NUM_REACTIONS]; // Whether each rate has a specified gradient
 
-    model(bool using_gradients = false, bool using_perturb=false) :
-      _using_perturb(using_perturb),
-      _using_gradients(using_gradients) {
-      for (int i = 0; i < NUM_REACTIONS; i++) {
-        factors_perturb[i] = 0.0;
-        _has_gradient[i] = false;
-        factors_gradient[i] = NULL;
-      }
-    }
+
     
 #define REACTION(name) reaction<name> reaction_##name;
     #include "reactions_list.hpp"
