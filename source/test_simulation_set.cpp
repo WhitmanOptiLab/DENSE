93799204da0e99103fd9bff22fc94106f8ff90eb
Kirk Lange <kirklange@users.noreply.github.com>
2017-05-31 11:37:15 -0700

    Added cmd args to old tests

--- source/test_simulation_set.cpp
+++ source/test_simulation_set.cpp
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
     arg_parse::init(argc, argv);
     
     simulation_set sim_set(
-        arg_parse::get<bool>("g", false, "gradients"),
-        arg_parse::get<bool>("p", false, "perturb"),
-        arg_parse::get<string>("mf", "../models/her_model_2014/param_list.csv", "model_file"),
-        arg_parse::get<int>("ct", 200, "cell_total"),
-        arg_parse::get<int>("tw", 50, "total_width"),
-        arg_parse::get<RATETYPE>("ss", 0.01, "step_size") );
+        arg_parse::get<bool>("G", false, "gradients"),
+        arg_parse::get<bool>("P", false, "perturb"),
+        arg_parse::get<string>("p", "../models/her_model_2014/param_list.csv", "param-list"),
+        arg_parse::get<int>("c", 200, "cell-total"),
+        arg_parse::get<int>("w", 50, "total-width"),
+        arg_parse::get<RATETYPE>("s", 0.01, "step-size") );
     sim_set.simulate_sets(arg_parse::get<int>("t", 60, "time"));
 }
