d1273dbf29fb213389b13b19d0bc8dc4c474bf4b
John Stratton <strattja@whitman.edu>
2017-06-19 10:03:29 -0700

    Added dense concentration level data structure files   Unused, but there for reference in case we need experimental comparison

---
+++ source/dense_cl.hpp
@@ -0,0 +1,127 @@
+#ifndef DENSE_CL_HPP
+#define DENSE_CL_HPP
+#include "specie.hpp"
+#include "model.hpp"
+#define WRAP(x, y) ((x) + (y)) % (y)
+#define MAX(x, y) ((x) < (y) ? (y) : (x))
+using namespace std;
+
+#include <cstddef>
+
+class simulation;
+
+
+class dense_cl {
+    //FIXME - want to make this private at some point
+  protected:
+    const simulation& _sim;
+    //const model& _model;
+    int   _length, _width,_total_length;
+    RATETYPE *_array;
+//    RATETYPE *_darray;
+
+    int _max_delay = 0;
+    int _specie_size = 0;
+
+  public:
+    class cell{
+    public:
+        
+        CPUGPU_FUNC
+        cell(RATETYPE *row): _array(row) {}
+        
+        CPUGPU_FUNC
+        RATETYPE& operator[](int k){
+            return _array[k];
+        }
+        
+        CPUGPU_FUNC
+        const RATETYPE& operator[](int k) const {
+            return _array[k];
+        }
+        RATETYPE *_array;
+    };
+    
+    
+    class timespan{
+    public:
+        CPUGPU_FUNC
+        timespan(RATETYPE *plane,int width, int pos): _array(plane), _width(width),_pos(pos) {};
+        
+        CPUGPU_FUNC
+        cell operator[](int j) {
+            j = WRAP(j, _pos);
+            cell temp(_array+_width*j);
+            return temp;
+        }
+        
+        CPUGPU_FUNC
+        const cell operator[](int j) const{
+            j = WRAP(j, _pos);
+            cell temp(_array+_width*j);
+            return temp;
+        }
+        RATETYPE *_array;
+        int _width;
+        int _pos;
+    };
+    
+    dense_cl(simulation& sim)
+    :_width(0), _total_length(0),_sim(sim){
+        allocate_array();
+    }
+    
+    dense_cl(int length, int width, simulation& sim)
+    :_width(width),_total_length(0),_sim(sim){
+        allocate_array();
+    }
+    
+    void initialize();
+    void reset(){
+        for (int i = 0; i < _total_length; i++) {
+            _array[i] = 0.0; // Initialize every concentration level at every time step for every cell to 0
+        }
+    }
+    
+    
+    CPUGPU_FUNC
+    timespan operator[](int i) {
+        return timespan(_array+(i*_sim._cells_total), _width, _max_delay);
+    }
+
+    CPUGPU_FUNC
+    const timespan operator[](int i) const {
+        return timespan(_array+(i*_sim._cells_total), _width, _max_delay);
+    }
+    
+    
+    
+    int width() const {return _width;}
+    int total_length() const {return _total_length;}
+    ~dense_cl() {
+      dealloc_array();
+    }
+    
+    
+protected:
+    void dealloc_array(){
+        if (_array){
+            delete[] _array;
+        }
+        _array= NULL;
+    }
+    
+    void allocate_array(){
+        if (_total_length >0){
+            _array= new RATETYPE[_total_length];
+            //if (_array == NULL){std::cout<<"ERROR"<<std::endl; exit(EXIT_MEMORY_ERROR);}
+        }
+        else{
+            _array= NULL;
+        }
+    }
+    
+};
+
+#endif
+
