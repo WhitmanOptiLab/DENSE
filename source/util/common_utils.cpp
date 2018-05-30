22819f2cf01c57072e72ad26cd36e6093484fee8
Maxwell Brown <maxwell@maxwellnbrown.com>
2018-05-30 13:05:39 -0700

    Move `left_pad` and `file_add_num` to 'source/main.cpp'

--- source/util/common_utils.cpp
+++ source/util/common_utils.cpp
@@ -1,21 +1,5 @@
 #include "common_utils.hpp"
 
-std::string file_add_num (
-  std::string file_name, std::string const& prefix,
-  char padding, unsigned file_no,
-  std::size_t padded_size, std::string const& extension_sep)
-{
-  auto padded_file_no = left_pad(std::to_string(file_no), padded_size, padding);
-  auto before_extension_sep = std::min(file_name.find_last_of(extension_sep), file_name.size());
-  file_name.insert(before_extension_sep, prefix + padded_file_no);
-  return file_name;
-};
-
-std::string left_pad (std::string string, std::size_t min_size, char padding) {
-  string.insert(string.begin(), min_size - std::min(min_size, string.size()), padding);
-  return string;
-};
-
 #include <algorithm>
 
 specie_vec str_to_species(std::string pSpecies)
