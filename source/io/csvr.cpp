e58ff3cb17dbe69d3971fa6de7f942272f073ba9
Maxwell Brown <maxwell@maxwellnbrown.com>
2018-06-01 10:34:53 -0700

    Refactor `csvr::get_next(RATETYPE *)`

--- source/io/csvr.cpp
+++ source/io/csvr.cpp
@@ -3,6 +3,7 @@
 
 #include <cfloat> // For FLT_MAX as an internal error code
 #include <cmath>
+#include <limits>
 #include <iostream>
 
 
@@ -30,88 +31,63 @@ bool csvr::get_next(int* rate) {
 }
 
 bool csvr::get_next(RATETYPE* pnRate) {
-    // Only bother if open
-    if (iFile.is_open())
-    {
-        // tParam data from file to be "pushed" to pfRate
-        std::string tParam;
+  // Only bother if open
+  if (!iFile.is_open()) {
+      std::cout << color::set(color::RED) << "CSV parsing failed. "
+          "No CSV file found/open." << color::clear() << '\n';
+      return false;
+  }
+  // tParam data from file to be "pushed" to pfRate
+  std::string tParam;
 
-        char c = iFile.get();
-        while(iFile.good())
+  char c;
+  while (iFile >> c) {
+      // Skip line comments
+      if (c == '#') {
+          iFile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          tParam.clear();
+          ++iLine;
+      }
+      // Parse only if not whitespace except for \n
+      else if (c != ' ' && c != '\t')
+      {
+        // Ignore non-numeric, non data seperator, non e, -, and + characters
+        if (!( (c >= '0' && c <= '9') || c == '.' || c == ',' || c == '\n' ||
+                c == 'e' || c == '-' || c == '+') )
         {
-            // Check if in comment
-            if (c == '#')
-            {
-                // Skip comment line
-                iFile.ignore(unsigned(-1), '\n');
-                tParam.clear();
-                iLine++;
-            }
-            // Parse only if not whitespace except for \n
-            else if (c != ' ' && c != '\t')
-            {
-                // Ignore non-numeric, non data seperator, non e, -, and + characters
-                if (!( (c >= '0' && c <= '9') || c == '.' || c == ',' || c == '\n' ||
-                        c == 'e' || c == '-' || c == '+') )
-                {
-                    iFile.ignore(unsigned(-1), ',');
-                    tParam.clear();
-                }
-                else
-                {
-                    // If hit data seperator, add data to respective array
-                    // '\n' is there in case there is no comma after last item
-                    if (c == ',' || c == '\n')
-                    {
-                        // Only push if tParam contains something
-                        if (tParam.length() > 0)
-                        {
-                            char* tInvalidAt;
-                            RATETYPE tRate = strtold(tParam.c_str(), &tInvalidAt);
-
-                            // If found invalid while parsing
-                            if (*tInvalidAt)
-                            {
-                                std::cout << color::set(color::RED) <<
-                                    "CSV parsing failed. Invalid data contained "
-                                    "at line " << iLine << "." <<
-                                    color::clear() << '\n';
-                                return false;
-                            }
-                            // Else was success
-                            else
-                            {
-                                pnRate != 0 ? *pnRate = tRate : 0;
-                                return true;
-                            }
-                        }
-                    }
-                    // Append if it is numbers or decimal
-                    else
-                    {
-                        tParam += c;
-                    }
-                }
-            }
-
-            // Increment line counter
-            if (c == '\n')
-            {
-                iLine++;
-            }
-
-            // Get next char in file
-            c = iFile.get();
+            iFile.ignore(std::numeric_limits<std::streamsize>::max(), ',');
+            tParam.clear();
         }
+        // If hit data seperator, add data to respective array
+        // '\n' is there in case there is no comma after last item
+        else if (c != ',' && c != '\n') {
+          tParam += c;
+        }
+        else if (!tParam.empty()) {
+          char* tInvalidAt;
+          RATETYPE tRate = strtold(tParam.c_str(), &tInvalidAt);
 
-        // End of file
-        return false;
-    }
-    // If failed to open current_ifstream
-    else
-    {
-        std::cout << color::set(color::RED) << "CSV parsing failed. "
-            "No CSV file found/open." << color::clear() << '\n';
-        return false;
-    }
+          // If found invalid while parsing
+          if (*tInvalidAt)
+          {
+              std::cout << color::set(color::RED) <<
+                  "CSV parsing failed. Invalid data contained "
+                  "at line " << iLine << "." <<
+                  color::clear() << '\n';
+              return false;
+          }
+          // Else was success
+          if (pnRate) { *pnRate = tRate; }
+          return true;
+        }
+      }
+
+      // Increment line counter
+      if (c == '\n') {
+          ++iLine;
+      }
+  }
+
+  // End of file
+  return false;
 }
