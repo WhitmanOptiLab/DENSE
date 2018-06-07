fa6e5edb4e938ea8c6e316a5bd81c4440aa4be8d
Maxwell Brown <maxwell@maxwellnbrown.com>
2018-06-07 15:29:57 -0700

    Prevent 'warning: extra ";" ignored' when compiling with NVCC

--- source/sim/determ/baby_cl.hpp
+++ source/sim/determ/baby_cl.hpp
@@ -25,15 +25,15 @@ class baby_cl {
   public:
     class cell{
     public:
-        
+
         IF_CUDA(__host__ __device__)
         cell(RATETYPE *row): _array(row) {}
-        
+
         IF_CUDA(__host__ __device__)
         RATETYPE& operator[](int k){
             return _array[k];
         }
-        
+
         IF_CUDA(__host__ __device__)
         const RATETYPE& operator[](int k) const {
             return _array[k];
@@ -44,20 +44,20 @@ class baby_cl {
     template <typename NumericT>
     static NumericT wrap (NumericT x, NumericT y) {
       return (x + y) % y;
-    };
+    }
 
     class timespan{
     public:
         IF_CUDA(__host__ __device__)
         timespan(RATETYPE *plane,int width, int pos, int hist_len): _array(plane), _width(width),_pos(pos),_hist_len(hist_len) {};
-        
+
         IF_CUDA(__host__ __device__)
         cell operator[](int j) {
             j = (j == 0) ? _pos : wrap(_pos + j, _hist_len);
             cell temp(_array+_width*j);
             return temp;
         }
-        
+
         IF_CUDA(__host__ __device__)
         const cell operator[](int j) const{
             j = (j == 0) ? _pos : wrap(_pos + j, _hist_len);
@@ -69,7 +69,7 @@ class baby_cl {
         int _pos;
         int _hist_len;
     };
-    
+
     baby_cl(simulation_determ& sim)
     :_width(0), _total_length(0),_sim(sim){
         allocate_array();
@@ -77,7 +77,7 @@ class baby_cl {
           _j[i] = 0;
         }
     }
-    
+
     baby_cl(int length, int width, simulation_determ& sim)
     :_width(width),_total_length(0),_sim(sim){
         allocate_array();
@@ -85,7 +85,7 @@ class baby_cl {
           _j[i] = 0;
         }
     }
-    
+
     void initialize();
     void reset(){
         for (int i = 0; i < _total_length; i++) {
@@ -95,8 +95,8 @@ class baby_cl {
           _j[i] = 0;
         }
     }
-    
-    
+
+
     IF_CUDA(__host__ __device__)
     timespan operator[](int i) {
         return timespan(_array+_position[i], _width, _j[i], _specie_size[i]);
@@ -106,22 +106,22 @@ class baby_cl {
     const timespan operator[](int i) const {
         return timespan(_array+_position[i], _width, _j[i], _specie_size[i]);
     }
-    
+
     IF_CUDA(__host__ __device__)
-    void advance() { 
+    void advance() {
       for (int i = 0; i < NUM_SPECIES; i++) {
         _j[i] = wrap(_j[i]+1, _specie_size[i]);
       }
     }
-    
-    
+
+
     int width() const {return _width;}
     int total_length() const {return _total_length;}
     ~baby_cl() {
       dealloc_array();
     }
-    
-    
+
+
 protected:
     void dealloc_array(){
         if (_array){
@@ -129,7 +129,7 @@ protected:
         }
         _array= NULL;
     }
-    
+
     void allocate_array(){
         if (_total_length >0){
             _array= new RATETYPE[_total_length];
@@ -138,10 +138,9 @@ protected:
             _array= NULL;
         }
     }
-    
+
 };
 
 
 
 #endif
-
