5904cdd4e8b1da6360119b14be69fea12174e58e
Maxwell Brown <maxwell@maxwellnbrown.com>
2018-06-08 12:13:26 -0700

    Move `PickyObserver` from 'core/observable.hpp' to 'anlys/base.hpp'
    Prepare to merge `PickyObserver` with `Analysis`

--- source/anlys/base.hpp
+++ source/anlys/base.hpp
@@ -7,11 +7,35 @@
 
 #include <memory>
 
-/*
-* Superclass for Analysis Objects
-* - observes passed "Observable"
-* - does not implement any analysis
-*/
+
+/// Observer that restricts updates to a specific range of cells and times
+/// Used as a stepping-stone to refactoring the Observer/Observable interface
+class PickyObserver : public Observer {
+
+  public:
+
+    PickyObserver(Observable & observable, int min, int max, RATETYPE start_time, RATETYPE end_time);
+
+    void when_updated_by(Observable &) override;
+
+    void when_unsubscribed_from(Observable &) override;
+
+    virtual void update(ContextBase& start) = 0;
+
+    virtual void finalize() = 0;
+
+  protected:
+
+    Real start_time, end_time;
+
+    int min, max;
+
+};
+
+
+/// Superclass for Analysis Objects
+/// - observes passed "Observable"
+/// - does not implement any analysis
 class Analysis : public PickyObserver {
 
   public:
--- source/core/observable.cpp
+++ source/core/observable.cpp
@@ -15,20 +15,6 @@ void Observable::finalize() {
   }
 }
 
-PickyObserver::PickyObserver(
-  Observable & observable,
-  int min, int max,
-  RATETYPE start_time,
-  RATETYPE end_time
-) :
-  min{min},
-  max{max},
-  start_time{start_time},
-  end_time{end_time}
-{
-  subscribe_to(observable);
-}
-
 void Observer::subscribe_to(Observable & observable) {
   subscriptions_.emplace_back(observable);
   observable.subscribers_.emplace_back(*this);
@@ -50,15 +36,4 @@ std::vector<std::reference_wrapper<Observable>> const& Observer::subscriptions()
 }
 
 void Observer::when_updated_by(Observable & observable) {
-}
-
-void PickyObserver::when_updated_by(Observable & observable) {
-  if (observable.t < start_time || observable.t >= end_time) return;
-  ContextBase & begin = *observable.context;
-  begin.set(min);
-  update(begin);
-}
-
-void PickyObserver::when_unsubscribed_from(Observable & observable) {
-  finalize();
 }
