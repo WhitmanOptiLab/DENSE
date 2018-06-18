eedf44f2b1d8c6643d79efaf356296844c79ac30
Maxwell Brown <maxwell@maxwellnbrown.com>
2018-06-18 11:58:44 -0700

    Move `csvw_param` to 'csv_gen.cpp'
    Eventually turn `csvw_param` into a function
    Currently it has a single constructor with side effects

--- source/csv_gen.cpp
+++ source/csv_gen.cpp
@@ -1,9 +1,18 @@
-#include "io/csvw_param.hpp"
 #include "utility/style.hpp"
 
 #include <cstdlib>
 #include <iostream>
 #include <string>
+
+#include "io/csvw.hpp"
+
+enum class param_type {
+  SETS, PERT, GRAD
+};
+
+struct csvw_param : private csvw {
+  csvw_param(std::string const& pcfFileName, param_type const& pcfType);
+};
 
 int main (int argc, char* argv[]) {
   if (argc < 2) {
@@ -18,4 +27,150 @@ int main (int argc, char* argv[]) {
   csvw_param( dir + "param_pert_template.csv", param_type::PERT );
   csvw_param( dir + "param_grad_template.csv", param_type::GRAD );
   return EXIT_SUCCESS;
+}
+
+
+#include "utility/style.hpp"
+#include "core/specie.hpp"
+#include "core/reaction.hpp"
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+namespace {
+
+    const std::string cReaction[] = {
+        #define REACTION(name) #name,
+        #define DELAY_REACTION(name) #name,
+        #include "reactions_list.hpp"
+        #undef REACTION
+        #undef DELAY_REACTION
+    };
+
+    const std::string cDelay[] = {
+        #define REACTION(name)
+        #define DELAY_REACTION(name) "dreact_" #name,
+        #include "reactions_list.hpp"
+        #undef REACTION
+        #undef DELAY_REACTION
+    };
+
+    const std::string cCritical[] = {
+        #define SPECIE(name)
+        #define CRITICAL_SPECIE(name) "rcrit_" #name,
+        #include "specie_list.hpp"
+        #undef SPECIE
+        #undef CRITICAL_SPECIE
+    };
+
+}
+
+csvw_param::csvw_param(std::string const& pcfFileName, param_type const& pcfType) :
+    csvw::csvw(pcfFileName, true, "\n# Rename this file by removing the "
+            "\"_template\" from the file name (or just change the name "
+            "entirely) once the data has been entered!\n")
+{
+
+    csvw & out = *this;
+
+    // param_type string prefix
+    std::vector<std::string> nPrefix;
+    std::string param_type_str;
+    switch (pcfType)
+    {
+        case param_type::SETS:
+            out << ("# This file can contain more than one "
+                    "set (each being on their own line). All sets "
+                    "are initialized and executed in parallel when a file is "
+                    "loaded into the simulation.\n");
+            out << ("# For more information and examples see README.md "
+                    "section 2.2.1\n\n");
+            param_type_str = "sets";
+            nPrefix = { "" };
+            break;
+        case param_type::PERT:
+            out << ("# This file should only contain one set of "
+                    "perturbations. Only this one perturbations set is applied "
+                    "to all parameter sets when a simulation set is being run.\n");
+            out << ("# Use \'0\' to indicate that a "
+                    "reaction should not have perturbations.\n");
+            out << ("# For more information and examples see README.md "
+                    "section 2.2.2\n\n");
+            param_type_str = "perturbations";
+            nPrefix = { "pert_" };
+            break;
+        case param_type::GRAD:
+            out << ("# This file should only contain one set of "
+                    "gradients. Only this one gradients setting is applied "
+                    "to all parameter sets when a simulation set is being run.\n");
+            out << ("# Use \'0\' under all four columns of a reaction "
+                    "to indiate that it should not have a gradient.\n"
+                    "# Gradient Codes\n"
+                    "#   x1 - start column\n"
+                    "#   y1 - start multiplier (use \'1.23\' to mean \'123%\')\n"
+                    "#   x2 - end column\n"
+                    "#   y2 - end multiplier\n");
+            out << ("# For more information and examples see README.md "
+                    "section 2.2.2\n\n");
+            param_type_str = "gradients";
+            nPrefix = { "grad_x1_", "grad_y1_", "grad_x2_", "grad_y2_" };
+            break;
+        default: throw std::out_of_range("Invalid param_type: " + static_cast<int>(pcfType));
+    }
+
+    // Write column headers
+    std::string zeros_line = "\n";
+    for (unsigned int i = 0; i < NUM_REACTIONS; ++i) {
+        for (auto & prefix : nPrefix) {
+            out << prefix << cReaction[i] << ", ";
+            zeros_line += "0, ";
+        }
+        // Add extra comma between gradient params for readability
+        if (pcfType == param_type::GRAD) {
+            out << ", ";
+            zeros_line += ", ";
+        }
+    }
+
+    out << ", ";
+    zeros_line += ", ";
+
+    for (unsigned int i = 0; i < NUM_DELAY_REACTIONS; ++i)
+    {
+        for (auto & prefix : nPrefix) {
+            out << prefix << cDelay[i] << ", ";
+            zeros_line += "0, ";
+        }
+
+        if (pcfType == param_type::GRAD) {
+            out << ", ";
+            zeros_line += ", ";
+        }
+    }
+
+    out << ", ";
+    zeros_line += ", ";
+
+    for (unsigned int i = 0; i < NUM_CRITICAL_SPECIES; i++) {
+        for (auto & prefix : nPrefix) {
+            out << prefix << "rcrit_" << cCritical[i] << ", ";
+            zeros_line += "0, ";
+        }
+
+        if (pcfType == param_type::GRAD) {
+            out << ", ";
+            zeros_line += ", ";
+        }
+    }
+
+
+    if (pcfType == param_type::PERT || pcfType == param_type::GRAD) {
+      out << (zeros_line);
+    }
+
+    std::cout << style::apply(Color::green) <<
+      "CSV parameter " << param_type_str <<
+      " column header generation successful. " <<
+      "See \'" << pcfFileName << "\'.\n" << style::reset();
 }
