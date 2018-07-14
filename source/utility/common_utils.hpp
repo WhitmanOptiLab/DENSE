ba5e934b7132d182ad24bff5c49d6751d139ab62
Maxwell Brown <maxwell@maxwellnbrown.com>
2018-07-13 17:31:58 -0700

    Implement shim for C++14's `std::make_unique` (in namespace std14)

--- source/utility/common_utils.hpp
+++ source/utility/common_utils.hpp
@@ -3,9 +3,20 @@
 
 #include "core/specie.hpp"
 #include "utility/numerics.hpp"
+#include <memory>
+
 // converts comma-separated list of specie names to specie_vec
 specie_vec str_to_species(std::string pcfSpecies);
 Real* parse_perturbations(std::string const& file_name);
 Real** parse_gradients(std::string const& file_name, int width);
+
+namespace std14 {
+
+  template <typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+
+}
 
 #endif
