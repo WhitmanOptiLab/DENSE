a6a2d27c5134ed94859cdbd2b44ba163eff2b51c
Maxwell Brown <maxwell@maxwellnbrown.com>
2019-04-23 19:51:07 -0700

    Reduce manual memory management in baby_cl

--- source/sim/determ/baby_cl.cpp
+++ source/sim/determ/baby_cl.cpp
@@ -6,10 +6,10 @@ dense::baby_cl::baby_cl(Deterministic_Simulation& sim)
 : _width{sim.cell_count()} {
     unsigned sum = 0;
     for (int i = 0; i < NUM_SPECIES; i++) {
-        _specie_size[i] = (sim.max_delays[i] / sim._step_size) + sim._num_history_steps;
         _position[i] = sum * _width;
+        _specie_size[i] = (sim.max_delays[i] / sim._step_size) + sim._num_history_steps;
         sum += _specie_size[i];
     }
     _total_length = sum * _width;
-    _array = new Real[_total_length]();
+    _array = decltype(_array){new Real[_total_length]()};
 }
