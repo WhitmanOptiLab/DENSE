ea6d950d272a6e6b22379118901b02623bc352d8
Maxwell Brown <maxwell@maxwellnbrown.com>
2019-04-23 19:51:07 -0700

    Reduce manual memory management in baby_cl

--- source/sim/determ/baby_cl.hpp
+++ source/sim/determ/baby_cl.hpp
@@ -4,11 +4,12 @@
 #include "utility/common_utils.hpp"
 #include "core/specie.hpp"
 #include "core/model.hpp"
+#include <memory>
 
 namespace dense {
 
 class Deterministic_Simulation;
-
+// Simulation_History
 class baby_cl {
 
   private:
@@ -16,9 +17,9 @@ class baby_cl {
     int _position[NUM_SPECIES];
     int _specie_size[NUM_SPECIES];
     int _j[NUM_SPECIES] = {};
-    int _length, _width;
+    int _width;
     unsigned _total_length;
-    Real *_array;
+    std::unique_ptr<Real[]> _array;
 
   public:
 
@@ -33,12 +34,12 @@ class baby_cl {
 public:
     Real* row_at(int species, int j) {
       j = (j == 0) ? _j[species] : wrap(_j[species] + j, _specie_size[species]);
-      return _array + _position[species] + _width * j;
+      return &_array[_position[species] + _width * j];
     }
     
     Real const* row_at(int species, int j) const {
       j = (j == 0) ? _j[species] : wrap(_j[species] + j, _specie_size[species]);
-      return _array + _position[species] + _width * j;
+      return &_array[_position[species] + _width * j];
     }
 
     CUDA_AGNOSTIC
@@ -46,10 +47,6 @@ public:
       for (int i = 0; i < NUM_SPECIES; i++) {
         _j[i] = wrap(_j[i]+1, _specie_size[i]);
       }
-    }
-
-    ~baby_cl() {
-      delete[] _array;
     }
 };
 
