c0b0158b0404152a521ddd552786f9956e40002e
Edwin Retana <edwinretana99@yahoo.com>
2020-06-09 10:58:28 -0700

    Adding in the new files to commit

---
+++ source/sim/determ/num_sim.hpp
@@ -0,0 +1,51 @@
+#ifndef SIM_DETERM_NUM_SIM_HPP
+#define SIM_DETERM_NUM_SIM_HPP
+
+#include "sim/base.hpp"
+#include "core/parameter_set.hpp"
+#include "core/model.hpp"
+#include "core/specie.hpp"
+#include "sim/cell_param.hpp"
+#include "core/reaction.hpp"
+#include "baby_cl.hpp"
+
+namespace dense {
+  typedef cell_param<NUM_DELAY_REACTIONS, int> IntDelays;
+
+  class Numerical_Integration {
+
+  public:
+    using SpecieRates = CUDA_Array<Real, NUM_SPECIES>;
+    IntDelays _intDelays;
+    Real _step_size;
+    int _j;
+    dense::Natural _num_history_steps; // how many steps in history are needed for this numerical method
+    Numerical_Integration(int num_delay_rxn, Natural& cell_cnt, Minutes& step_size, Deterministic_Simulation& sim);
+    Numerical_Integration(int num_delay_rxn, Natural& cell_cnt, Minutes& step_size, Simpson_Simulation& sim);
+    //Numerical_Integration(int num_delay_rxn, Natural& cell_cnt, Minutes& step_size, Deterministic_Simulation& sim);
+  protected:
+    baby_cl _baby_cl;
+  public:
+    Real get_concentration(dense::Natural cell, specie_id species) const {
+      return get_concentration(cell, species, 1);
+    }
+
+    Real get_concentration(dense::Natural cell, specie_id species, dense::Natural delay) const {
+      return _baby_cl.row_at(species, 1 - delay)[cell];
+    }
+
+    CUDA_AGNOSTIC
+    virtual void update_concentrations(dense::Natural cell, SpecieRates const& rates);
+
+    virtual void step() = 0;
+
+    CUDA_AGNOSTIC
+    virtual Minutes age_by(Minutes duration) = 0;
+
+    virtual dense::Real calculate_neighbor_average(dense::Natural cell,
+                specie_id species, dense::Natural delay = 0) const = 0;
+
+  };
+}
+
+#endif
---
+++ source/sim/determ/simpson.cpp
@@ -0,0 +1,109 @@
+#include <cmath>
+#include "simpson.hpp"
+#include "num_sim.hpp"
+#include "model_impl.hpp"
+#include "baby_cl.hpp"
+#include <limits>
+#include <iostream>
+#include <cassert>
+
+dense::Simpson_Simulation::Simpson_Simulation(const Parameter_Set& ps, Real* pnFactorsPert, Real** pnFactorsGrad,
+                    Minutes step_size, std::vector<Real> conc, NGraph::Graph adj_graph) :
+    Simulation(ps, std::move(adj_graph), pnFactorsPert, pnFactorsGrad),
+    Numerical_Integration(NUM_DELAY_REACTIONS, cell_count(), step_size, *this) {
+      //Copy and normalize _delays into _intDelays
+      for (int i = 0; i < NUM_DELAY_REACTIONS; i++) {
+        for (dense::Natural j = 0; j < cell_count(); ++j) {
+          _intDelays[i][j] = cell_parameters_[NUM_REACTIONS+i][j] / _step_size;
+        }
+      }
+      if (!conc.empty()) {
+        for (int specie = 0; specie < NUM_SPECIES; specie++) {
+          int specie_size = _baby_cl.get_species_size(specie);
+          for (int time = 0; time < specie_size; time++){
+            for(dense::Natural cell = 0; cell < cell_count(); cell++){
+              _baby_cl.row_at(specie,time)[cell] = conc[specie];
+            }
+          }
+        }
+      }
+    }
+
+void dense::Simpson_Simulation::step() {
+    for (dense::Natural k = 0; k < cell_count(); k++) {
+      update_concentrations(k, calculate_concentrations(k));
+    }
+    _j++;
+    _baby_cl.advance();
+}
+
+CUDA_AGNOSTIC
+void dense::Simpson_Simulation::update_concentrations(dense::Natural cell, SpecieRates const& rates) {
+  //TODO: Implement Simpson's rule in this function
+  //      it is currently left as the same implementation as Deterministic_Simulation,
+  //      just to see what method was previously used.
+
+    //Runs through and performs one step of Simpson's Rule to each specie
+    for (int i = 0; i < NUM_SPECIES; i++) {
+      //_curr_coeff is '1' for the first two steps, we just use Euler's Method
+      if (!_second_point_calculated) {
+        auto curr_rate = rates[i];
+        _baby_cl.row_at(i, 1)[cell] = _baby_cl.row_at(i, 0)[cell] + _step_size * curr_rate;
+      } else {
+        _baby_cl.row_at(i, 1)[cell] = _baby_cl.row_at(i, 0)[cell] + (_step_size/3)*((_curr_coeff-1)*_prev_rates[i] + rates[i]);
+        _prev_rates[i] = rates[i];
+      }
+    }
+
+    //Updating what has been done.
+    //These if-else statements are ordered in priority
+    if (!_first_point_calculated) {
+      _first_point_calculated = true;
+    } else if (!_second_point_calculated) {
+      _second_point_calculated = true;
+      _curr_coeff = 4;
+      for (int k = 0; k < NUM_SPECIES; k++) {
+        _prev_rates[k] = rates[k];
+      }
+    } else if (_curr_coeff == 4) {
+      _curr_coeff = 2;
+    } else {
+      _curr_coeff = 4;
+    }
+}
+
+
+CUDA_AGNOSTIC
+dense::Simpson_Simulation::SpecieRates dense::Simpson_Simulation::calculate_concentrations(dense::Natural cell) {
+    //Step 1: for each reaction, compute reaction rate
+    CUDA_Array<Real, NUM_REACTIONS> reaction_rates;
+    #define REACTION(name) reaction_rates[name] = dense::model::reaction_##name.active_rate(Context(*this, cell));
+        #include "reactions_list.hpp"
+    #undef REACTION
+
+    //Step 2: allocate specie concentration rate change array
+    SpecieRates specie_deltas{};
+
+    //Step 3: for each reaction rate, for each specie it affects, accumulate its contributions
+    #define REACTION(name) \
+    const reaction<name>& r##name = dense::model::reaction_##name; \
+    for (int j = 0; j < r##name.getNumDeltas(); j++) { \
+        specie_deltas[delta_ids_##name[j]] += reaction_rates[name]*deltas_##name[j]; \
+    }
+    #include "reactions_list.hpp"
+    #undef REACTION
+
+    return specie_deltas;
+}
+
+
+CUDA_AGNOSTIC
+Minutes dense::Simpson_Simulation::age_by (Minutes duration) {
+  //TODO: This should be able to stay as is, but confirm that the only real difference is in the definition of Step()
+  assert(duration > 0 && t > 0 && _step_size > 0);
+  dense::Natural steps = (duration /*+ std::remainder(t, _step_size)*/) / Minutes{ _step_size };
+  for (dense::Natural s = 0; s < steps; ++s) {
+    step();
+  }
+  return Simulation::age_by(duration);
+}
