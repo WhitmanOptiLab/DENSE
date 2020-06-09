586fdbe8a9fbb008043ed4f86c9de59683ed5c6c
Edwin Retana <edwinretana99@yahoo.com>
2020-06-09 10:58:28 -0700

    Adding in the new files to commit

---
+++ source/sim/determ/num_sim.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include "num_sim.hpp"
+#include "simpson.hpp"
+#include "model_impl.hpp"
+#include "baby_cl.hpp"
+#include <limits>
+#include <iostream>
+#include <cassert>
+
+dense::Numerical_Integration::Numerical_Integration(int num_delay_rxn, Natural& cell_cnt, Minutes& step_size, Deterministic_Simulation& sim) :
+        _intDelays(num_delay_rxn, cell_cnt),
+        _step_size{step_size / Minutes{1}}, _j(0), _num_history_steps(2), _baby_cl(sim) {
+
+}
+
+dense::Numerical_Integration::Numerical_Integration(int num_delay_rxn, Natural& cell_cnt, Minutes& step_size, Simpson_Simulation& sim) :
+        _intDelays(num_delay_rxn, cell_cnt),
+        _step_size{step_size / Minutes{1}}, _j(0), _num_history_steps(2), _baby_cl(sim) {
+
+}
---
+++ source/sim/determ/simpson.hpp
@@ -0,0 +1,51 @@
+#ifndef SIM_DETERM_SIMPSON_HPP
+#define SIM_DETERM_SIMPSON_HPP
+
+#include "num_sim.hpp"
+#include "sim/base.hpp"
+#include "core/parameter_set.hpp"
+#include "core/model.hpp"
+#include "core/specie.hpp"
+#include "sim/cell_param.hpp"
+#include "core/reaction.hpp"
+#include "baby_cl.hpp"
+
+namespace dense {
+
+  typedef cell_param<NUM_DELAY_REACTIONS, int> IntDelays;
+
+  class Simpson_Simulation : public Simulation, public Numerical_Integration {
+  private:
+    int _curr_coeff = 1;
+    bool _first_point_calculated = false;
+    bool _second_point_calculated = false;
+    double _prev_rates[NUM_SPECIES];
+  public:
+    using Context = dense::Context<Simpson_Simulation>;
+    Simpson_Simulation(const Parameter_Set& ps, Real* pnFactorsPert, Real** pnFactorsGrad,
+                    Minutes step_size, std::vector<Real> conc, NGraph::Graph adj_graph);
+
+    CUDA_AGNOSTIC
+    SpecieRates calculate_concentrations(dense::Natural cell);
+
+    void step() override;
+
+    dense::Real calculate_neighbor_average(dense::Natural cell, specie_id species, dense::Natural delay = 0) const override {
+      // Average the given cell's neighbors' concentrations
+      Real sum = 0.0L;
+      for (dense::Natural i = 0; i < neighbor_count_by_cell_[cell]; i++) {
+          sum += _baby_cl.row_at(species, -delay)[neighbors_by_cell_[cell][i]];
+      }
+      Real avg = sum / neighbor_count_by_cell_[cell];
+      return avg;
+    }
+
+    CUDA_AGNOSTIC
+    Minutes age_by (Minutes duration) override;
+
+    CUDA_AGNOSTIC
+    void update_concentrations(dense::Natural cell, SpecieRates const& rates) override;
+  };
+}
+
+#endif
