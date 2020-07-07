2e8add4492bd12b2af37fb16d9affb4f638a4b43
Edwin Retana <edwinretana99@yahoo.com>
2020-07-07 16:13:48 -0700

    Improved accuracy and functionality of numerical integration methods

--- source/sim/determ/simpson.cpp
+++ source/sim/determ/simpson.cpp
@@ -12,6 +12,12 @@ dense::Simpson_Simulation::Simpson_Simulation(const Parameter_Set& ps, Real* pnF
     Simulation(ps, std::move(adj_graph), pnFactorsPert, pnFactorsGrad),
     Numerical_Integration(NUM_DELAY_REACTIONS, cell_count(), step_size, *this) {
       //Copy and normalize _delays into _intDelays
+      std::vector<std::vector<double>>* v_1 = new std::vector<std::vector<double>> (cell_count(), std::vector<double> (NUM_SPECIES, 0));
+      std::vector<std::vector<double>>* v_2 = new std::vector<std::vector<double>> (cell_count(), std::vector<double> (NUM_SPECIES, 0));
+      _prev_rates = *v_1;
+      _last_rates = *v_2;
+      delete v_1;
+      delete v_2;
       for (int i = 0; i < NUM_DELAY_REACTIONS; i++) {
         for (dense::Natural j = 0; j < cell_count(); ++j) {
           _intDelays[i][j] = cell_parameters_[NUM_REACTIONS+i][j] / _step_size;
@@ -30,10 +36,31 @@ dense::Simpson_Simulation::Simpson_Simulation(const Parameter_Set& ps, Real* pnF
     }
 
 
+CUDA_AGNOSTIC
+Minutes dense::Simpson_Simulation::age_by (Minutes duration) {
+  //TODO: This should be able to stay as is, but confirm that the only real difference is in the definition of Step()
+  assert(duration > 0 && t > 0 && _step_size > 0);
+  dense::Natural steps = (duration /*+ std::remainder(t, _step_size)*/) / Minutes{ _step_size };
+  for (dense::Natural s = 0; s < steps; ++s) {
+    step();
+  }
+  return Simulation::age_by(duration);
+}
+
 
 void dense::Simpson_Simulation::step() {
     for (dense::Natural k = 0; k < cell_count(); k++) {
       update_concentrations(k, calculate_concentrations(k));
+    }
+    if (!_first_point_calculated) {
+      _first_point_calculated = true;
+    } else if (!_second_point_calculated) {
+      _second_point_calculated = true;
+      _curr_coeff = 4;
+    } else if (_curr_coeff == 4) {
+      _curr_coeff = 2;
+    } else {
+      _curr_coeff = 4;
     }
     _j++;
     _baby_cl.advance();
@@ -48,32 +75,23 @@ void dense::Simpson_Simulation::update_concentrations(dense::Natural cell, Speci
     //Runs through and performs one step of Simpson's Rule to each specie
     for (int i = 0; i < NUM_SPECIES; i++) {
       //_curr_coeff is '1' for the first two steps, we just use Euler's Method
-      auto curr_rate = rates[i];
+      double curr_rate = rates[i];
       if (!_second_point_calculated) {
         _baby_cl.row_at(i, 1)[cell] = _baby_cl.row_at(i, 0)[cell] + _step_size * curr_rate;
-        _prev_rates[i] = curr_rate;
-      } else {
-        auto next_rate_predicted = 2*curr_rate - _prev_rates[i];
-        if (_curr_coeff == 4) {
-          _baby_cl.row_at(i, 1)[cell] = _baby_cl.row_at(i, 0)[cell] + (_step_size/4) * ((_curr_coeff - 1) * curr_rate + next_rate_predicted);
-        } else {
-          _baby_cl.row_at(i, 1)[cell] = _baby_cl.row_at(i, 0)[cell] + (_step_size/2) * ((_curr_coeff - 1) * curr_rate + next_rate_predicted);
+        if (_first_point_calculated) {
+          _last_rates[cell][i] = _prev_rates[cell][i];
         }
-        _prev_rates[i] = curr_rate;
+        _prev_rates[cell][i] = curr_rate;
+
+      } else {
+        _baby_cl.row_at(i, 1)[cell] = _baby_cl.row_at(i, -1)[cell] + (_step_size/3)*(6.5*curr_rate - _prev_rates[cell][i] + 0.5*_last_rates[cell][i]);
+        _last_rates[cell][i] = _prev_rates[cell][i];
+        _prev_rates[cell][i] = curr_rate;
       }
     }
     //Updating what has been done.
     //These if-else statements are ordered in priority
-    if (!_first_point_calculated) {
-      _first_point_calculated = true;
-    } else if (!_second_point_calculated) {
-      _second_point_calculated = true;
-      _curr_coeff = 4;
-    } else if (_curr_coeff == 4) {
-      _curr_coeff = 2;
-    } else {
-      _curr_coeff = 4;
-    }
+
 }
 
 
@@ -98,16 +116,4 @@ dense::Simpson_Simulation::SpecieRates dense::Simpson_Simulation::calculate_conc
     #undef REACTION
 
     return specie_deltas;
-}
-
-
-CUDA_AGNOSTIC
-Minutes dense::Simpson_Simulation::age_by (Minutes duration) {
-  //TODO: This should be able to stay as is, but confirm that the only real difference is in the definition of Step()
-  assert(duration > 0 && t > 0 && _step_size > 0);
-  dense::Natural steps = (duration /*+ std::remainder(t, _step_size)*/) / Minutes{ _step_size };
-  for (dense::Natural s = 0; s < steps; ++s) {
-    step();
-  }
-  return Simulation::age_by(duration);
 }
