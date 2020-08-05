e41274b85a283703402f1489e637727a2f264476
edwin-retana <edwinretana99@yahoo.com>
2020-08-05 15:50:50 -0700

    Renaming average simulation to ODEpack simulation

--- source/runtimecheck.hpp
+++ source/runtimecheck.hpp
@@ -1,10 +1,5 @@
 //
 //  runtimecheck.hpp
-<<<<<<< HEAD
-//
-=======
-//  
->>>>>>> 4b573d7b2417b481c38dccb7190aad21747d155b
 //
 //  Created by Myan Sudharsanan on 7/14/20.
 //
@@ -29,11 +24,6 @@ class runtimecheck{
         double duration;
         std::chrono::high_resolution_clock::time_point begin;
         std::chrono::high_resolution_clock::time_point end;
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 4b573d7b2417b481c38dccb7190aad21747d155b
         std::vector<double> durations;
         std::vector<std::chrono::high_resolution_clock::time_point> beginnings;
         std::vector<std::chrono::high_resolution_clock::time_point> endings;
@@ -42,47 +32,28 @@ class runtimecheck{
             begin = std::chrono::high_resolution_clock::now();
             beginnings.emplace_back(begin);
         }
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 4b573d7b2417b481c38dccb7190aad21747d155b
         void set_end(){
             end = std::chrono::high_resolution_clock::now();
             endings.emplace_back(end);
         }
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 4b573d7b2417b481c38dccb7190aad21747d155b
         void set_begin(){
             begin = std::chrono::high_resolution_clock::now();
             beginnings.emplace_back(begin);
         }
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 4b573d7b2417b481c38dccb7190aad21747d155b
         double get_duration(int i, int j){
             duration = std::chrono::duration_cast<std::chrono::microseconds>( endings[i] - beginnings[j] ).count();
             durations.emplace_back(duration);
             return duration;
         }
-<<<<<<< HEAD
 
         /*std::vector<double> duration_list (){
             return durations;
         }*/
 
-=======
-    
         /*std::vector<double> duration_list (){
             return durations;
         }*/
-    
->>>>>>> 4b573d7b2417b481c38dccb7190aad21747d155b
+
         virtual ~runtimecheck() = default;
         runtimecheck(runtimecheck&&)  = default;
 };
